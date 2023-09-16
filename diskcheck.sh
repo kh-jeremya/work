5b3a1832570a1dc3e5130d7b6d1fa63d9981782a
kh-jeremya <jarmstrong@knownhost.com>
2023-09-16 10:21:53 -0500

    Modding diskcheck 8

--- diskcheck.sh
+++ diskcheck.sh
@@ -2,7 +2,7 @@
 
 alias diskover=`/bin/df -a -T -h -t ext4`
 alias diskbreakdown="/usr/bin/du -shc / 2>&1 | /usr/bin/grep -v  '^du:' | /usr/bin/grep G"
-alias 50files=`/usr/bin/find /home -type f -size +50M -exec ls -lah {} + | /usr/bin/awk {'print $9,"  - ", $5'}`
+alias 50files= "/usr/bin/find /home -type f -size +50M -exec ls -lah {} + | /usr/bin/awk {'print $9, $5'}"
 
 echo '=========='
 echo 'Disk Usage Overview'
