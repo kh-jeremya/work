6e4096339bab6c176122463f5a031f05d9314268
kh-jeremya <jarmstrong@knownhost.com>
2023-09-16 10:25:04 -0500

    Modding diskcheck 11

--- diskcheck.sh
+++ diskcheck.sh
@@ -1,11 +1,12 @@
 /bin/bash
 
-alias diskbreakdown="/usr/bin/du -shc / 2>&1 | /usr/bin/grep -v  '^du:' | /usr/bin/grep G"
-alias 50files="/usr/bin/find /home -type f -size +50M -exec ls -lah {} + | /usr/bin/awk {'print $9, $5'}"
+diskover="/bin/df -a -T -h -t ext4"
+diskbreakdown="/usr/bin/du -shc / 2>&1 | /usr/bin/grep -v  '^du:' | /usr/bin/grep G"
+50files="/usr/bin/find /home -type f -size +50M -exec ls -lah {} + | /usr/bin/awk {'print $9, $5'}"
 
 echo '=========='
 echo 'Disk Usage Overview'
-/bin/df -a -T -h -t ext4
+$diskover
 printf '\n\n'
 echo 'Disk Usage Breakdown'
 $diskbreakdown
