99b1338846596769c15c89ddbfd3b2eb0afc782f
kh-jeremya <jarmstrong@knownhost.com>
2023-09-16 10:25:43 -0500

    Modding diskcheck 12

--- diskcheck.sh
+++ diskcheck.sh
@@ -2,7 +2,7 @@
 
 diskover="/bin/df -a -T -h -t ext4"
 diskbreakdown="/usr/bin/du -shc / 2>&1 | /usr/bin/grep -v  '^du:' | /usr/bin/grep G"
-50files="/usr/bin/find /home -type f -size +50M -exec ls -lah {} + | /usr/bin/awk {'print $9, $5'}"
+fiftyfiles="/usr/bin/find /home -type f -size +50M -exec ls -lah {} + | /usr/bin/awk {'print $9, $5'}"
 
 echo '=========='
 echo 'Disk Usage Overview'
@@ -12,5 +12,5 @@ echo 'Disk Usage Breakdown'
 $diskbreakdown
 printf '\n\n'
 echo 'Files 50M or larger in /home'
-$50files
+$fiftyfiles
 echo '=========='
