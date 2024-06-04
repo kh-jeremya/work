c8f6014df8cd5e44b7deaa8206fc1691cf43f1d6
[KnownHost JeremyA] <jarmstrong@knownhost.com>
2024-06-04 13:49:09 -0500

    elevate-phpcheck 1-04Jun

--- elevate-phpcheck.sh
+++ elevate-phpcheck.sh
@@ -1,10 +1,11 @@
-printf "php56:\n\n" > sites.list
-whmapi1 php_get_vhosts_by_version version=ea-php56 | grep "-" >> sites.list
-printf "\n\nphp70:\n\n" >> sites.list
-whmapi1 php_get_vhosts_by_version version=ea-php70 | grep "-" >> sites.list
-printf "\n\nphp71:\n\n" >> sites.list
-whmapi1 php_get_vhosts_by_version version=ea-php71 | grep "-" >> sites.list
-printf "\n\nphp72:\n\n" >> sites.list
-whmapi1 php_get_vhosts_by_version version=ea-php72 | grep "-" >> sites.list
-printf "\n\nphp73:\n\n" >> sites.list
-whmapi1 php_get_vhosts_by_version version=ea-php73 | grep "-" >> sites.list
+printf "php56:\n\n" > el_php-sites.list
+whmapi1 php_get_vhosts_by_version version=ea-php56 | grep "-" >> el_php-sites.list
+printf "\n\nphp70:\n\n" >> el_php-sites.list
+whmapi1 php_get_vhosts_by_version version=ea-php70 | grep "-" >> el_php-sites.list
+printf "\n\nphp71:\n\n" >> el_php-sites.list
+whmapi1 php_get_vhosts_by_version version=ea-php71 | grep "-" >> el_php-sites.list
+printf "\n\nphp72:\n\n" >> el_php-sites.list
+whmapi1 php_get_vhosts_by_version version=ea-php72 | grep "-" >> el_php-sites.list
+printf "\n\nphp73:\n\n" >> el_php-sites.list
+whmapi1 php_get_vhosts_by_version version=ea-php73 | grep "-" >> el_php-sites.list
+echo 'file is '${PWD}'/el_php-sites.list'
